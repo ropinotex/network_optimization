6fdc7d0b96de2e59da8af63ac2ddda755378bbeb
Ropinot <roberto.pinto@exolvia.com>
2022-03-17 07:34:22 +0100

    modified:   data_structures.py 	modified:   scenario_1.py 	modified:   scenario_2.py

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,16 @@
+# ==============================================================================
+# description     :Optimization models for teaching purposes
+# author          :Roberto Pinto
+# date            :2022.03.22
+# version         :1.0
+# notes           :This software is meant for teaching purpose only and it is provided as-is under the GPL license.
+#                  The models are inspired by the book Watson, M., Lewis, S., Cacioppi, P., Jayaraman, J. (2013)
+#                  Supply Chain Network Design, Pearson. 
+#                  http://networkdesignbook.com/
+#                  All the data has been taken from the book.
+#                  The software is provided as-is, with no guarantee by the author.
+# ==============================================================================
+
 from collections import namedtuple
 Warehouse = namedtuple('Warehouse', 'name, city, state, zipcode, latitude, longitude')
 Customer = namedtuple('Customer', 'name, city, state, zipcode, latitude, longitude, demand')
