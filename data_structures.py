0ac3f3904495b6b1120e4db5f40b42c3ba0e985c
Ropinot <roberto.pinto@exolvia.com>
2022-04-04 13:12:07 +0200

    modified:   Netopt.ipynb 	modified:   WarehouseNetworkOptimizationWithCapacities.ipynb 	modified:   data_structures.py

--- data_structures.py
+++ data_structures.py
@@ -12,6 +12,86 @@
 # ==============================================================================
 
 from collections import namedtuple
+from math import sqrt
+import pandas as pd
+
+
 Warehouse = namedtuple('Warehouse', 'name, city, state, zipcode, latitude, longitude, capacity')
 Customer = namedtuple('Customer', 'name, city, state, zipcode, latitude, longitude, demand')
 
+def import_data(data, datatype):
+    """ Importa data from a variable. 
+        The data parameter must be a list of strings containing values separated by ';'
+        The data must be in this order:
+            - Warehouse: "IDENTIFIER;X_COORD;Y_COORD;CAPACITY"
+            - Customer: "IDENTIFIER;X_COORD;Y_COORD;DEMAND"
+        Use -1 to represent infinite capacity
+        Parameter datatype refers to either warehouse or customer
+        It returns a variable containing all the data that can be used in the optimization function"""
+
+    if datatype not in ['warehouse', 'customer']:
+        raise Exception('Parameter datatype must be either warehouse or customer')
+    if not isinstance(data, list):
+        raise Exception('Parameter data must be a list of either warehouse or customer data')
+
+    imported_data = {}
+
+    for n, each in enumerate(data):
+        row = each.split(';')
+
+        if row[3] in [-1, '-1']:
+            q = None
+        else:
+            q = float(row[3])
+
+        if datatype == 'warehouse':
+            imported_data[n] = Warehouse(name=row[0],
+                                         city="",
+                                         state="",
+                                         zipcode="",
+                                         latitude=float(row[2]),
+                                         longitude=float(row[1]),
+                                         capacity=q)
+        elif datatype == 'customer':
+            imported_data[n] = Customer(name=row[0],
+                                        city="",
+                                        state="",
+                                        zipcode="",
+                                        latitude=float(row[2]),
+                                        longitude=float(row[1]),
+                                        demand=q)
+    
+    return imported_data
+
+def dist(origin, destination):
+    """ Return the distance between origin and destination.
+        origin must be of type Warehouse, whereas destination must be of type Customer"""
+
+    if isinstance(origin, Warehouse) and isinstance(destination, Customer):
+        return sqrt((origin.latitude - destination.latitude)**2 + (origin.longitude - destination.longitude)**2)
+
+
+def calculate_dm(warehouses=None, customers=None):
+    """ Calculate the distance matrix between warehouses and customers"""
+
+    if not all([warehouses, customers]):
+        raise Exception('You must pass the location of warehouses and customers')
+    
+    dm = {}
+
+    for kw, w in warehouses.items():
+        for kc, c in customers.items():
+            dm[(kw, kc)] = dist(w, c)
+
+    return dm
+
+def show_data(data):
+    """ Print the data in a readable format """
+    if not isinstance(data, dict):
+        raise Exception('Param data must be a dict')
+    df = []
+    for k, v in data.items():
+        df.append(list(v))
+
+    df = pd.DataFrame(df, columns=['Id', 'City', 'State', 'Zipcode', 'x', 'y', 'Capcity/Demand'])
+    return df
